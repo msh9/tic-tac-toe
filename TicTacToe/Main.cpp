f47b08fb99944c9cb2423ac3de28b7e86c331736
Michael Hughes <michael@mihughes.com>
2016-04-24 10:06:10 -0600

    Add basic textures, and implement basic click detection

--- TicTacToe/Main.cpp
+++ TicTacToe/Main.cpp
@@ -1,28 +1,58 @@
+#include "Main.h"
+#include <memory> 
+#include <iostream>
 #include <SFML/Graphics.hpp>
 
 using namespace sf;
 int main() {
-	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML Stuff");
-	sf::CircleShape shape(100.f);
-	shape.setFillColor(sf::Color::Green);
+	sf::RenderWindow window(sf::VideoMode(1410, 910), "SFML Stuff");
 
+	/// Begin load textures
+	Texture boardTexture;
+	boardTexture.loadFromFile("resources/tic-tac-toe-board.png");
+	Texture circleTexture;
+	circleTexture.loadFromFile("resources/tic-tac-toe-circle.png");
+	Texture xTexture;
+	xTexture.loadFromFile("resources/tic-tac-toe-x.png");
+	/// End load textures
+	/// Begin create base sprites
+	auto board = Sprite(boardTexture);
+	auto circle = Sprite(circleTexture);
+	circle.setPosition(Vector2f(950.f, 0.f));
+	auto x = Sprite(xTexture);
+	x.setPosition(Vector2f(950.f, 320.f));
+	/// End create base sprites
 
 	while (window.isOpen()) {
+		//Base sprites are always drawn first
+		window.clear();
+		window.draw(board);
+		window.draw(circle);
+		window.draw(x);
+
 		sf::Event event;
 		while (window.pollEvent(event)) {
-			if (event.type == sf::Event::Closed) {
-				window.close();
+			switch (event.type) {
+			case Event::Closed: window.close();
+				break;
+			case Event::MouseButtonPressed: handleMouseClick(&event, &circle, &x);
+				break;
 			}
 		}
-
-		window.clear();
-		window.draw(shape);
 		window.display();
-
 	}
 	return 0;
 }
 
-RectangleShape getBoard(int xSize, int ySize) {
-
+void handleMouseClick(Event* event, Sprite* circle, Sprite* x) {
+	if (event->mouseButton.button == Mouse::Left) {
+		FloatRect circleBounds = circle->getGlobalBounds();
+		if (circleBounds.contains((float)event->mouseButton.x, (float)event->mouseButton.y)) {
+			std::cout << "Mouse clicked in circle" << std::endl;
+		}
+		FloatRect xBounds = x->getGlobalBounds();
+		if (xBounds.contains((float)event->mouseButton.x, (float)event->mouseButton.y)) {
+			std::cout << "Mouse clicked in x" << std::endl;
+		}
+	}
 }
---
+++ TicTacToe/Main.h
@@ -0,0 +1,8 @@
+#pragma once
+#ifndef __Main__
+#define __Main__
+#include <SFML\Graphics.hpp>
+#include <memory>
+
+void handleMouseClick(sf::Event*, sf::Sprite* , sf::Sprite* );
+#endif // !__Main__
